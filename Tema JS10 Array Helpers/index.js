c3ba70d73aee591addeacf32364f618bb3f13a6b
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-07-14 23:08:19 +0300

    rezolvare final

--- Tema JS10 Array Helpers/index.js
+++ Tema JS10 Array Helpers/index.js
@@ -1,22 +1,22 @@
-/*Creaza o funcție care primeste 2 parametri, un array, si un numar, si returneaza un alt array care reprezinta numerele din primul array care sunt divizibile cu numarul specificat.
+/* 1. Creaza o funcție care primeste 2 parametri, un array, si un numar, si returneaza un alt array care reprezinta numerele din primul array care sunt divizibile cu numarul specificat.
 Ex: 
   Input: filtreazaDivizibile([1,2,3,4,5,6,7,8], 2) 
   Output [2,4,6,8] */
 
-// function filtreazaNumerele(array,nr){
-//     let numereFiltrate=[]
-//     array.forEach(function(arr){
-//         if(arr%nr==0){
-//             numereFiltrate.push(arr)
-//         }
-//     })
-//     console.log(numereFiltrate)
-// }
+function filtreazaNumerele(array,nr){
+    let numereFiltrate=[]
+    array.forEach(function(arr){
+        if(arr%nr==0){
+            numereFiltrate.push(arr)
+        }
+    })
+    console.log(numereFiltrate)
+}
 
-// filtreazaNumerele([1,2,3,4,5,6,7,8],2)
+filtreazaNumerele([1,2,3,4,5,6,7,8],2)
 
 
-/*Creaza o functie care primeste ca parametru 2 array-uri, primul va fi un array cu mai multe
+/* 2. Creaza o functie care primeste ca parametru 2 array-uri, primul va fi un array cu mai multe
  numere, al doilea un array cu mai putine numere. Scopul functiei este sa afle daca toate elementele
   din array-ul mic sunt prezente in array-ul mare, si sa returneze true sau daca nu sunt, sa returneze false.
 
@@ -30,24 +30,29 @@ Exemple:
   Input: verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [10, 13]) 
   Output false */
 
-// function verificaIncluziuneArray(arrMare,arrMic){
-//     let raspuns;
-//     arrMic.forEach(function(el){
-//         // console.log(el)
-//         if(arrMare.includes(el)){
-//            return raspuns=true;
-//         }else{
-//             return raspuns=false;
-//         }
-//     })
-//     console.log(raspuns)
-// }
+function verificaIncluziuneArray(arrMare,arrMic){
+    let raspuns;
+    arrMic.forEach(function(el){
+        // console.log(el)
+        if(arrMare.includes(el)){
+           return raspuns=true;
+        }else{
+            return raspuns=false;
+        }
+    })
+    console.log(raspuns)
+}
 
-// verificaIncluziuneArray ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3]);
-// verificaIncluziuneArray([1,2,3,4,5,6,7,8], [1, 2, 13])
-// verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [10, 13]) 
+verificaIncluziuneArray ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3]);
+verificaIncluziuneArray([1,2,3,4,5,6,7,8], [1, 2, 13])
+verificaIncluziuneArray ([1,2,3,4,5,6,7,8], [10, 13]) 
 
 
+/* 3. Creaza o functie care sterge un element dintr-un array. Functia primeste atat array-ul cat si elementul care trebuie gasit si sters ca parametri, si returneaza functia noua.
+
+Exemple:
+  Input: eliminaElement([1,2,3,4], [ 3]) 
+  Output [1,2,4 ] */
 
 function stergeElement(arr,element){
   let newArr=[]
@@ -61,4 +66,3 @@ function stergeElement(arr,element){
 }
 
 stergeElement([1,2,3,4], [3]) 
-/* */
